b795687e0271ec561c97ccd9e1e15ec0aeeaed21
Jack <151611438@qq.com>
2019-01-27 12:20:44 +0800

    Initial Commit

---
+++ JXLivePush/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JXLivePush
+//
+//  Created by 许伟杰 on 2019/1/27.
+//  Copyright © 2019 JackXu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
