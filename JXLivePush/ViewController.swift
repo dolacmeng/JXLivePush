b484b50e6f6be3681e76c54a4b17d8070e087121
Jack <151611438@qq.com>
2019-01-27 13:20:42 +0800

    init

--- JXLivePush/ViewController.swift
+++ JXLivePush/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import LFLiveKit
 
 class ViewController: UIViewController {
-
+    //MARK: - Getters and Setters
+    lazy var session: LFLiveSession = {
+        let audioConfiguration = LFLiveAudioConfiguration.default()
+        let videoConfiguration = LFLiveVideoConfiguration.defaultConfiguration(for: .low3, outputImageOrientation: .portrait)
+        let session = LFLiveSession(audioConfiguration: audioConfiguration, videoConfiguration: videoConfiguration)
+        
+        session?.preView = self.view
+        return session!
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func startLive(_ sender: UIButton) {
+        let stream = LFLiveStreamInfo()
+        stream.url = "rtmp://192.168.1.104:1935/rtmplive/demo";
+        session.startLive(stream)
+        session.running = true
+    }
+
+    @IBAction func stopLive(_ sender: UIButton) {
+        session.stopLive()
+    }
 
 
 }
-
---
+++ Pods/Target Support Files/Pods-JXLivePush/Pods-JXLivePush-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JXLivePush : NSObject
+@end
+@implementation PodsDummy_Pods_JXLivePush
+@end
